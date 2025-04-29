36371b7287276c097a2c9710753b5006d6ebbecf
ahmed abdalla <ahmed.amin.abd.elwadod@gmail.com>
2025-04-29 13:44:45 +0200

    feat(auth): enhance SetPasswordDto with password matching validation and updated password criteria

--- apps/hr-core-server/src/modules/auth/dto/set-password.dto.ts
+++ apps/hr-core-server/src/modules/auth/dto/set-password.dto.ts
@@ -1,10 +1,35 @@
-import { IsString, MinLength } from 'class-validator';
+import {
+  IsString,
+  Matches,
+  Validate,
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+@ValidatorConstraint({ name: 'MatchPassword', async: false })
+class MatchPasswordConstraint implements ValidatorConstraintInterface {
+  validate(confirmPassword: string, args: ValidationArguments) {
+    const object = args.object as { password: string };
+    return object.password === confirmPassword;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `password does not match password.`;
+  }
+}
 
 export class SetPasswordDto {
   @IsString()
   token!: string;
 
   @IsString()
-  @MinLength(8)
+  @Matches(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/, {
+    message:
+      'Password must be at least 8 characters long, contain 1 uppercase letter, and 1 number',
+  })
   password!: string;
+
+  @IsString()
+  @Validate(MatchPasswordConstraint)
+  confirmPassword!: string;
 }
