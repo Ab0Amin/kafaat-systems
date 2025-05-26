f6475b7e5431502ef7f9bc7dec087661f609ebb3
ahmed abdalla <ahmed.amin.abd.elwadod@gmail.com>
2025-05-26 15:49:01 +0200

    refactor(middleware): rename register-tenant middleware file and remove unused tenant middleware

--- libs/hr/core-app/src/lib/core/infrastructure/midilwares/index.ts
+++ libs/hr/core-app/src/lib/core/infrastructure/midilwares/index.ts
@@ -1,7 +1,7 @@
 import { SubdomainMiddleware } from './subdomain.middleware';
 import { RequestMethod } from '@nestjs/common';
 // import { TenantMiddleware } from './tenant.middlewar';
-import { RegisterTenantMiddleware } from './register-tenant.middlewar';
+import { RegisterTenantMiddleware } from './register-tenant.middleware';
 
 export const MIDDLEWARES = [
   {
